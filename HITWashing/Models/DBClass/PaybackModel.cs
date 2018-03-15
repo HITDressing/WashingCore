06d01654fcc6e1d36be0dcef7f4d9d6708ddc005
昊燃 王 <lbwanghr@outlook.com>
2018-03-15 16:30:17 +0800

    payback

---
+++ HITWashing/Models/DBClass/PaybackModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HITWashing.Models.DBClass
+{
+    public class PaybackModel
+    {
+        [Key]
+        [DisplayName("POID")]
+        public int PaybackOrderID { get; set; }
+
+        public int UserID { get; set; }
+    }
+}
