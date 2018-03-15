cbac01d9d8b85450e89a95cede978c3d9519920c
Scarlet <1996scarlet@gmail.com>
2018-03-15 15:50:14 +0800

    3-15-sx

---
+++ HITWashing/Models/DBClass/BalanceModel.cs
@@ -0,0 +1,20 @@
+﻿using HITWashing.Models.EnumClass;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HITWashing.Models.DBClass
+{
+    public class BalanceModel
+    {
+        [Key]
+        [DisplayName("BID")]
+        public int BalanceID { get; set; }
+
+        [DisplayName("余额")]
+        public double Balance { get; set; }
+
+        public int AccountID { get; set; }
+        public virtual AccountModel Account { get; set; }
+    }
+}
