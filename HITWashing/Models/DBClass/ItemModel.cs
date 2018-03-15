8a8a109419bd1e05d1ebf27b3d3ed4ac6ea0a4a3
昊燃 王 <lbwanghr@outlook.com>
2018-03-15 15:49:46 +0800

    fuck

---
+++ HITWashing/Models/DBClass/ItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HITWashing.Models.DBClass
+{
+    public class ItemModel
+    {
+        [Key]
+        [DisplayName("物品编号")]
+        public int ItemID { get; set; }
+
+        [Required]
+        [DisplayName("物品名称")]
+        public string ItemName { get; set; }
+
+        [Required]
+        [DisplayName("物品价值")]
+        public double ItemValue { get; set; }
+
+    }
+}
