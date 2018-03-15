77cc33626ee0fb8d1d8ba746d741c3016524ef98
Scarlet <1996scarlet@gmail.com>
2018-03-15 15:14:08 +0800

    3-15-sx

---
+++ HITWashing/Models/DBClass/AccountModel.cs
@@ -0,0 +1,50 @@
+﻿using HITWashing.Models.EnumClass;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HITWashing.Models.DBClass
+{
+    public class AccountModel
+    {
+        [Key]
+        [DisplayName("AID")]
+        public int AccountID { get; set; }
+
+        [DisplayName("手机号")]
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(11, ErrorMessage = "电话号码长度必须为11位", MinimumLength = 11)]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        [DisplayName("坐标X")]
+        public double LocationX { get; set; }
+
+        [Required]
+        [DisplayName("坐标Y")]
+        public double LocationY { get; set; }
+
+        [Required]
+        [DisplayName("用户类别")]
+        public EnumAccountType Type { get; set; }
+
+        [DisplayName("用户密码")]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "密码长度必须在8到32之间", MinimumLength = 8)]
+        [Required]
+        public string Password { get; set; }
+
+        [DisplayName("密码确认")]
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "密码长度必须在8到32之间", MinimumLength = 8)]
+        public string ConfirmPassword { get; set; }
+
+        [DisplayName("混淆盐")]
+        public string Salt { get; set; }
+
+        [DisplayName("商户名")]
+        public string StoreName { get; set; }
+    }
+}
---
+++ HITWashing/Models/EnumClass/WashingEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace HITWashing.Models.EnumClass
+{
+    public enum EnumAccountType
+    {
+        超级管理员,
+        仓库保管员,
+        财务负责人,
+        配送专员,
+        客户
+    }
+}
