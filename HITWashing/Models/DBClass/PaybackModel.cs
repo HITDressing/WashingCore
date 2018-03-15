febfb4314fc4533c5076a244bcff638027742da3
昊燃 王 <lbwanghr@outlook.com>
2018-03-15 16:37:30 +0800

    payback completed

--- HITWashing/Models/DBClass/PaybackModel.cs
+++ HITWashing/Models/DBClass/PaybackModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +15,18 @@ namespace HITWashing.Models.DBClass
         public int PaybackOrderID { get; set; }
 
         public int UserID { get; set; }
+        [ForeignKey("UserID")]
+        public virtual AccountModel UAccount { get; set; }
+
+        public int TransportID { get; set; }
+        [ForeignKey("TransportID")]
+        public virtual AccountModel TAccount { get; set; }
+
+        [DisplayName("是否取消")]
+        public bool IsCanceled { get; set; }
+
+        [DisplayName("是否完成")]
+        public bool IsCompleted { get; set; }
+
     }
 }
